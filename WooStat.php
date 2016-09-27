6314215c50d789ff898db4362fcce1a14532fa78
Saleh <animatorsaleh@gmail.com>
2016-09-27 11:50:00 +0330

    Total Paid amount for completed orders added

--- WooStat.php
+++ WooStat.php
@@ -1,14 +1,14 @@
 <?php
 
 /**
- * Plugin Name: WooStat
+ * Plugin Name: orders statistics
  * Plugin sirsaleh.com
- * Description: This Plugin Made to give a simple statistics about orders of Woocommerce.
+ * Description: This Plugin Made to give a simple statistics about orders.
  * Author: SirSaleh
  * Author URI: sirsaleh.com
  * Version: 1.0.1
  *
- * 
+ * Copyright: (c) 2015-2015 SkyVerge, Inc. (info@skyverge.com)
  *
  * License: GNU General Public License v3.0
  * License URI: http://www.gnu.org/licenses/gpl-3.0.html
@@ -26,6 +26,7 @@ if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
 require_once('orders_of_product.php');
 require_once('total_amount_of.php');
+require_once('total-paid.php');
 
 
 function my_plugin_menu() {
@@ -55,12 +56,13 @@ function my_plugin_options() {
 		$loop = new WP_Query( $args );
 		//var_dump($loop);
 		if ( $loop->have_posts() ) {
-			echo ("<table><tr><td style='border: 1px solid black;' >Product Name</td><td style='border: 1px solid black;' >Product Id</td><td style='border: 1px solid black;' >Count of the orders</td><td style='border: 1px solid black;' >Total Counts of orders</td></tr>");
+			echo ("<table><tr><td style='border: 1px solid black;' >Product Name</td><td style='border: 1px solid black;' >Product Id</td><td style='border: 1px solid black;' >Count of the orders</td><td style='border: 1px solid black;' >Total Counts of orders</td><td style='border: 1px solid black;' >Total Paid for orders</td></tr>");
 			while ( $loop->have_posts() ) : $loop->the_post();
 				if ($loop->post->_stock_status == 'instock'){
 					echo ("<tr>");
 					$sum = 0;
 					$total_sum=0;
+					$total_paid = 0;
 					echo ("<th style='border-left: 1px solid black;' >".$loop->post->post_title." </th><th style='border-left: 1px solid black;' >".$loop->post->ID."</th>");
 					$ord = get_orders_of($loop->post->ID);
 					//var_dump($ord);
@@ -70,10 +72,11 @@ function my_plugin_options() {
 								$ord->the_post();
 								if ($ord->post->post_status == "wc-completed"){
 									$total_sum = $total_sum + get_total_count($ord->post->ID,$loop->post->post_title);
+									$total_paid = $total_paid + get_total_paid($ord->post->ID,$loop->post->post_title) ;
 									$sum++;
 								}
 							}
-							echo ("<th style='border-left: 1px solid black;' >".$sum."</th><th style='border-left: 1px solid black;' >".$total_sum."</th>");
+							echo ("<th style='border-left: 1px solid black;' >".$sum."</th><th style='border-left: 1px solid black;' >".$total_sum."</th><th style='border-left: 1px solid black;' >".$total_paid."</th>");
 						}else{
 							echo ("<th>0</th><th>0</th>");
 						}
